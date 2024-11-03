ecab8c002425fa19cd620edda26ecd7619c47dfe
Sebastian Anklamm <sanklamm@gmail.com>
2024-11-03 14:37:38 +0100

    fix typo

--- models/user.go
+++ models/user.go
@@ -6,10 +6,10 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	Name      string    `gorm:"size:255" json:"name"`
-	Email     string    `gorm:"unique;size:255" json:"email"`
-	Passoword string    `gorm:"size:255" json:"-"`
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Name     string    `gorm:"size:255" json:"name"`
+	Email    string    `gorm:"unique;size:255" json:"email"`
+	Password string    `gorm:"size:255" json:"-"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
